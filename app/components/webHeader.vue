c1539aa0fc7cef7d2a832895ef3e63da70b5940a
lixiaonong <nonglixiao@dzsinfotech.com>
2025-07-30 19:25:00 +0800

    头部开发初成（响应式布局进行中）

--- app/components/webHeader.vue
+++ app/components/webHeader.vue
@@ -1,13 +1,103 @@
 <template>
-    <header>
-        <h2>头部</h2>
-    </header>
+    <div class="headerBox">
+        <el-row class="header" justify="end">
+            <el-image :src="headerLogo" fit="contain" />
+            <el-col :sm="24" :md="16" :lg="22">
+                <el-row class="header" justify="end">
+                    <a class="header-button" href="https://levinlaw.com/free-case-review/" target="_self">Free Case
+                        Review</a>
+                    <div class="header-nav flex-center" v-for="item in navList" :key="item.name" :class="{'active': item.active}">
+                        <a href="#" v-if="item.name !== 'Search'">{{ item.name }}</a>
+                        <a href="#" class="search" v-else>
+                            {{ item.name }}
+                        </a>
+                    </div>
+                </el-row>
+            </el-col>
+        </el-row>
+
+
+    </div>
+
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import headerLogo from '../assets/headerLogo.png'
+
+const navList = [
+    {
+        name: 'Our Firm',
+        active: true,
+    },
+    {
+        name: 'Attorneys',
+    },
+    {
+        name: 'Practice Areas',
+    },
+    {
+        name: 'Government',
+    },
+    {
+        name: 'News',
+    },
+    {
+        name: 'Co-Counsel',
+    },
+    {
+        name: '(800) 277-1193',
+    },
+    {
+        name: 'Search',
+        active: true,
+    },
+]
 
 </script>
 
 <style lang="scss" scoped>
+.headerBox {
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    .header {
+        background-color: #043450;
+        position: relative;
+        .header-button {
+            background-color: #007db3;
+            color: #fff;
+            font-family: Hind Madurai, sans-serif;
+            font-size: 22px;
+            font-weight: 700;
+            line-height: 49px;
+            margin-right: 8px;
+            padding: 10px 38px;
+        }
+        .header-nav {
+                font-size: 20px;
+                line-height: 46px;
+                padding: 10px 38px;
+                line-height: 1;
+                &.active {
+                    background-color: #007db3;
+                }
+                a {
+                    color: #fff;
+                    font-family: Hind Madurai, sans-serif;
+                    font-weight: 700;
+                }
+                .search {
+                  font-size: 18px;
+                  display: flex;
+                  flex-direction: column;
+                  &::before {
+                    content: url('../assets/whiteSearch.svg');
+                    text-align: center;
+                  }
+                }
+            }
+    }
+}
 
 </style>
